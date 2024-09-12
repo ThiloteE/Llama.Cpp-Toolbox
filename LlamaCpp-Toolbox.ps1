8707fadbc067b974b62d3d7690b3039efc398f80
3simplex <10260755+3Simplex@users.noreply.github.com>
2024-09-12 14:06:09 -0400

    Update version to test toolbox update.

--- LlamaCpp-Toolbox.ps1
+++ LlamaCpp-Toolbox.ps1
@@ -3,7 +3,7 @@
 # This also acts as the toolbox environment setup script.
 
 # Llama.cpp-Toolbox version
-$global:version = "0.26.y"
+$global:version = "0.26.x"
 
 # The directory where LlamaCpp-Toolbox.ps1 is initialized.
 $global:path = $PSScriptRoot 
