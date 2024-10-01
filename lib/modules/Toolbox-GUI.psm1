806e42a231ebe42131a266be60513308e83a5283
3simplex <10260755+3Simplex@users.noreply.github.com>
2024-10-01 10:16:33 -0400

    Bump version.
    Signed-off-by: 3simplex <10260755+3Simplex@users.noreply.github.com>

--- lib/modules/Toolbox-GUI.psm1
+++ lib/modules/Toolbox-GUI.psm1
@@ -4,7 +4,7 @@
 # todo # Assist with update of Transformers
 
 # Llama.cpp-Toolbox GUI version
-$global:version_GUI = "0.1.1"
+$global:version_GUI = "0.1.2"
 
 Add-Type -AssemblyName System.Windows.Forms
 
